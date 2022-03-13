191fce6a526d50047ef7622e8f34cd99d365fce1
IanWanyeki <gi.wanyeki@gmail.com>
2022-03-12 18:44:31 -0800

    Question 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char char = "and that piece of art is useful" - Dora Korpar, 2015-10-19\n";
+putchar(char);
+return 0;
+}
